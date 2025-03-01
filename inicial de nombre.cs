9cf5866ace14f45fd75126b2c2b4f2a91f75fc8c
Josue67l <josueguerrero85314@gmail.com>
2025-02-28 22:54:50 -0400

    Add files via upload

---
+++ inicial de nombre.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduce tu nombre: ");
+        string nombre = Console.ReadLine();
+
+        if (!string.IsNullOrWhiteSpace(nombre))
+        {
+            char inicial = nombre.Trim()[0];
+            Console.WriteLine($"Tu inicial es: {inicial}");
+        }
+        else
+        {
+            Console.WriteLine("No ingresaste un nombre válido.");
+        }
+    }
+}
