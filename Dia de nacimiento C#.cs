0feefbedd20cc057c4b34cfc88e394cf2b24ab24
Josue67l <josueguerrero85314@gmail.com>
2025-02-28 22:24:26 -0400

    Add files via upload

---
+++ Dia de nacimiento C#.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduce tu fecha de nacimiento (dd/mm/yyyy): ");
+        string input = Console.ReadLine();
+
+        if (DateTime.TryParseExact(input, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime fecha))
+        {
+            string diaSemana = fecha.ToString("dddd", new CultureInfo("es-ES"));
+            Console.WriteLine($"Naciste un {diaSemana}.");
+        }
+        else
+        {
+            Console.WriteLine("Formato de fecha inválido.");
+        }
+    }
+}
